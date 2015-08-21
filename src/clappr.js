59c9be2e2ba39b12326a149d299f34d4f78a54ca
Washington Soares <washingtonudi@gmail.com>
2015-08-21 16:40:45 -0300

    first commit

---
+++ src/clappr.js
@@ -0,0 +1,31 @@
+'use strict';
+
+angular.module('clappr')
+  .directive('clappr', function () {
+    return {
+      restrict: 'E',
+      scope: {
+        'src' : '='
+      },
+      template: '<div id="player"></div>',
+      link: function(scope, element, attrs) {
+        scope.$watch('src', function(newValue, oldValue) {
+          if (!newValue)
+            return;
+
+          angular.element('#player').children().remove();
+
+          var locationImage = "http://"+window.location.host + window.location.pathname + "images/logo_watermark.png";
+
+          new Clappr.Player({
+            source: scope.src,
+            parentId: "#player",
+            autoPlay: true,
+            watermark: locationImage,
+            width: "100%"
+          });
+          scope.$$watchers = [];
+        });
+      }
+    };
+  });
