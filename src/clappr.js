61f9731fabff8f52b3f545c216b09ebde557c444
Samir Machado <samircomputacao@hotmail.com>
2016-02-26 12:05:45 -0300

    novos updates

--- src/clappr.js
+++ src/clappr.js
@@ -7,7 +7,7 @@ angular.module('clappr',[])
     scope: {
       'src' : '='
     },
-    template: '<div id="player"></div>',
+    template: '<div class="embed-responsive embed-responsive-16by10"><div id="player" class="embed-responsive-item"></div></div>',
     link: function(scope, element, attrs) {
       scope.$watch('src', function(newValue, oldValue) {
         if (!newValue)
@@ -15,7 +15,15 @@ angular.module('clappr',[])
 
         angular.element('#player').children().remove();
 
-        var locationImage = "http://"+window.location.host + window.location.pathname + "images/logo_watermark.png";
+        var locationImage = "http://" + window.location.host + window.location.pathname + "images/logo_watermark.png";
+
+        $("#player").resizable({
+          aspectRatio: 16/9,
+          maxHeight: 720,
+          maxWidth: 1280,
+          minHeight: 240,
+          minWidth: 320
+        });
 
         new Clappr.Player({
           source: scope.src,
@@ -23,7 +31,7 @@ angular.module('clappr',[])
           autoPlay: false,
           watermark: locationImage,
           width: "100%",
-          height: "456px"
+          height: "100%" // 456px"
         });
 
         angular.element('video')[0].onended = function() {
